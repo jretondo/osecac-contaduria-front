ff64c05e4b678e6858495b2cfb5268a4cc6825f2
Javier Retondo <JRetondo@osecac.dns>
2023-05-08 14:40:57 -0300

    change

--- src/views/admin/bankStatements/components/parameters/components/list/modalTypes/orderType.js
+++ src/views/admin/bankStatements/components/parameters/components/list/modalTypes/orderType.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Col, Row } from 'reactstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, Col, Form, Row, Spinner } from 'reactstrap';
 import IndividualTypeOrder from './individualTypeOrder';
+import ActionsBackend from 'context/actionsBackend';
+import API_ROUTES from '../../../../../../../../api/routes';
+import AlertsContext from 'context/alerts';
 
 const OrderTypesModal = ({
     item,
@@ -9,12 +12,18 @@ const OrderTypesModal = ({
     newType
 }) => {
     const [orderList, setOrderList] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    const { axiosPut } = useContext(ActionsBackend)
+    const { newAlert } = useContext(AlertsContext)
 
     useEffect(() => {
         const lastOrder = dataPage.length > 0 ? dataPage[dataPage.length - 1].order : 0
-        setOrderList([...dataPage, {
+        const newList = dataPage.length > 0 ? dataPage.map(item => ({ ...item, original: item.order })) : []
+        setOrderList([...newList, {
             order: lastOrder + 1,
-            name: newType
+            name: newType,
+            original: null
         }])
     }, [newType, dataPage])
 
@@ -50,39 +59,69 @@ const OrderTypesModal = ({
         setOrderList(newList)
     }
 
-    return (<>
-        <Row>
-            <Col>
-                <h2 style={{ textAlign: "center" }}>Ordene el nuevo tipo</h2>
-            </Col>
-        </Row>
-        {
-            orderList.length > 0 &&
-            orderList.map((item, key) => {
-                return (<IndividualTypeOrder
-                    key={key}
-                    name={item.name}
-                    order={item.order}
-                    color={item.active}
-                    up={() => toUp(item.order)}
-                    down={() => toDown(item.order)}
-                    last={orderList.length - 1}
-                />)
-            })
+    const applyChanges = async () => {
+        const data = {
+            orderList
         }
-        <Row style={{ marginTop: "20px" }}>
-            <Col style={{ textAlign: "center" }}>
-                <Button color="danger"
-                    onClick={e => {
-                        e.preventDefault()
-                        setModalActive(1)
-                    }}
-                >Volver</Button>
-                <Button
-                    type="submit"
-                    color="success">Siguiente</Button>
-            </Col>
-        </Row>
+        setLoading(true)
+        const response = await axiosPut(API_ROUTES.bankStatementsDir.sub.types, data)
+        if (!response.error) {
+            newAlert("success", "Movimientos actualizados!", "Los movimientos fueron actualizados con éxito!")
+        } else {
+            newAlert("danger", "Hubo un error al querer extraer los saldos", "error: " + response.errorMsg)
+        }
+        setLoading(false)
+    }
+
+    return (<>
+        <Form onSubmit={e => {
+            e.preventDefault()
+            applyChanges()
+        }}>
+            {
+                loading ?
+                    <Row>
+                        <Col md="12" style={{ textAlign: "center" }}>
+                            <Spinner style={{ with: "200px", height: "200px" }} />
+                        </Col>
+                    </Row>
+                    :
+                    <>
+                        <Row>
+                            <Col>
+                                <h2 style={{ textAlign: "center" }}>Ordene el nuevo tipo</h2>
+                            </Col>
+                        </Row>
+                        {
+                            orderList.length > 0 &&
+                            orderList.map((item, key) => {
+                                return (<IndividualTypeOrder
+                                    key={key}
+                                    name={item.name}
+                                    order={item.order}
+                                    color={item.active}
+                                    up={() => toUp(item.order)}
+                                    down={() => toDown(item.order)}
+                                    last={orderList.length - 1}
+                                />)
+                            })
+                        }
+                        <Row style={{ marginTop: "20px" }}>
+                            <Col style={{ textAlign: "center" }}>
+                                <Button color="danger"
+                                    onClick={e => {
+                                        e.preventDefault()
+                                        setModalActive(1)
+                                    }}
+                                >Volver</Button>
+                                <Button
+                                    type="submit"
+                                    color="success">Aplicar</Button>
+                            </Col>
+                        </Row>
+                    </>
+            }
+        </Form>
     </>)
 }
 
